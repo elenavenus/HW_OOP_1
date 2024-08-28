7fcef01d1fa3d56b681bd83958f946078f092af4
elena kolesnikova <elena_kolesnikov@list.ru>
2024-08-28 08:30:58 +0200

    =выполненное домашка

--- src/main/java/netologia/homework/Main.java
+++ src/main/java/netologia/homework/Main.java
@@ -4,14 +4,19 @@ package netologia.homework;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        FormDate birthday = new FormDate();
+        birthday.day = 28;
+        birthday.month = 1;
+        birthday.year = 1980;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Post post = new Post();
+        post.name = "Elena";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Викторовна";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Kolesnikova";
+        post.subscription = false;
+        post.birthday = birthday;
+
     }
 }
